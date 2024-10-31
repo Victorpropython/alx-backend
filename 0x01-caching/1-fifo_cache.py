252e067ac42e68487cd4f17755b782a61a698c11
victorpropython <victorchibuzor3109@gmail.com>
2024-10-31 05:25:13 +0100

    for task 2 AND 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""A class FIFOCache that inherits from BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A simple class for a firstin first out order of caching
+    """
+    def put(self, key, item):
+        """Assigns item value to the key in cache data evicting the
+        oldest item if necessary
+
+        Args:
+            key (str): The key to store the item under
+            item (any): The item to be store
+
+        Returns:
+            None
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                print("DISCARD:", first_key)
+                del self.cache_data[first_key]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves the item associated with the key from the cache
+
+        Args:
+            key (str): The key to retrievr the item from
+
+        Returns:
+            The value associated with the key, or None if the key is not
+            found or is None
+        """
+        if key is None or Key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Class to implement Last in First out
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class inherits from BaseCaching and implements a Last-In-First-Out (LIFO) caching system."""
+
+    def put(self, key, item):
+        """Assigns the item value to the key in the cache data, evicting the newest item if necessary.
+
+        Args:
+            key (str): The key to store the item under.
+            item (any): The item to store.
+
+        Returns:
+            None
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = list(self.cache_data.keys())[-1]  # Get the last key
+                print("DISCARD:", last_key)
+                del self.cache_data[last_key]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves the item associated with the key from the cache.
+
+        Args:
+            key (str): The key to retrieve the item from.
+
+        Returns:
+            The value associated with the key, or None if the key is not found or is None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
