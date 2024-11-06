91ac73362be11ba54942e03096e6991832798704
victorpropython <victorchibuzor3109@gmail.com>
2024-11-06 05:48:03 +0100

    for task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,7 +3,6 @@
     as a title page and Helloword as a Header
 """
 from flask import Flask, render_template
-from flask_babel import Babel, _
 
 
 app = Flask(__name__)
@@ -17,8 +16,7 @@ def index():
     Returns:
         To return hello world
     """
-    greeting = (_("Hello, World!"))
-    return render_template('index.html', greeting=greeting)
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
