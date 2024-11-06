61ea2630442a6a4a80ee4e1444a251c3de5401fa
victorpropython <victorchibuzor3109@gmail.com>
2024-11-06 15:51:55 +0100

    for task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""to determine the best match with our supported language"""
+
+
+from flask import Flask, request
+from flask_babel import Babel, LocalSelector
+
+app = Flask(__name__)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
